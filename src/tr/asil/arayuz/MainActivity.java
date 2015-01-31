5f68676f14576dacc010ed34f413be165f75d57c
Asil Arslan <asilarslan93@gmail.com>
2015-01-31 14:15:30 +0200

    update mainactivity

--- src/tr/asil/arayuz/MainActivity.java
+++ src/tr/asil/arayuz/MainActivity.java
@@ -77,55 +77,11 @@ public class MainActivity extends FragmentActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	private void changeColor(int newColor) {
-
-		tabs.setIndicatorColor(newColor);
-
-		// change ActionBar color just if an ActionBar is available
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-
-			Drawable colorDrawable = new ColorDrawable(newColor);
-			Drawable bottomDrawable = getResources().getDrawable(R.drawable.actionbar_bottom);
-			LayerDrawable ld = new LayerDrawable(new Drawable[] { colorDrawable, bottomDrawable });
-
-			if (oldBackground == null) {
-
-				if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
-					ld.setCallback(drawableCallback);
-				} else {
-					getActionBar().setBackgroundDrawable(ld);
-				}
-
-			} else {
-
-				TransitionDrawable td = new TransitionDrawable(new Drawable[] { oldBackground, ld });
-
-				if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
-					td.setCallback(drawableCallback);
-				} else {
-					getActionBar().setBackgroundDrawable(td);
-				}
-
-				td.startTransition(200);
-
-			}
-
-			oldBackground = ld;
-
-			// http://stackoverflow.com/questions/11002691/actionbar-setbackgrounddrawable-nulling-background-from-thread-handler
-			//getActionBar().setDisplayShowTitleEnabled(false);
-			// getActionBar().setDisplayShowTitleEnabled(true);
-
-		}
-
-		currentColor = newColor;
-
-	}
+	
 
 	public void onColorClicked(View v) {
-		//renk deðiþtir
-		//int color = Color.parseColor(v.getTag().toString());
-		//changeColor(color);
+		//renk seçimi
+		
 
 	}
 
@@ -139,25 +95,10 @@ public class MainActivity extends FragmentActivity {
 	protected void onRestoreInstanceState(Bundle savedInstanceState) {
 		super.onRestoreInstanceState(savedInstanceState);
 		currentColor = savedInstanceState.getInt("currentColor");
-		changeColor(currentColor);
+		
 	}
 
-	private Drawable.Callback drawableCallback = new Drawable.Callback() {
-		@Override
-		public void invalidateDrawable(Drawable who) {
-			getActionBar().setBackgroundDrawable(who);
-		}
-
-		@Override
-		public void scheduleDrawable(Drawable who, Runnable what, long when) {
-			handler.postAtTime(what, when);
-		}
-
-		@Override
-		public void unscheduleDrawable(Drawable who, Runnable what) {
-			handler.removeCallbacks(what);
-		}
-	};
+	
 
 	public class MyPagerAdapter extends FragmentPagerAdapter {
 
